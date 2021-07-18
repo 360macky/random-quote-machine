7062393bdfbc4ab485abacb6a1ccf52470bcadb2
Marcelo Arias <mail.marcelo.as@gmail.com>
2021-07-18 15:58:00 -0500

    color

--- index.js
+++ index.js
@@ -5,6 +5,21 @@
 // 102 <- Generate a number from 0 to 102
 
 
+let colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+];
+
 let btnClick=(e)=> {
     console.log('something')
     console.log(e);
@@ -14,10 +29,16 @@ let btnClick=(e)=> {
     // when you console log it
     // {...} (102)
 
+    // https://360macky.github.io/rqm/index.html
+
     fetch(QUOTES_API).then(response => response.json()).then(data => {
         let allInfo = data.quotes;
         const text = document.getElementById('text');
         const author = document.getElementById('author');
+        const body = document.getElementById('body');
+
+        // change the background with JS
+        body.style.backgroundColor = "Red";
 
         let randomNumber = generateRandomNumber();
 

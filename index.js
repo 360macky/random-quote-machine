ceb72627515dee7c94ee1cdf6ec84aad6df1a0e2
Marcelo Arias <mail.marcelo.as@gmail.com>
2021-07-18 16:58:15 -0500

    hashtag

--- index.js
+++ index.js
@@ -18,23 +18,26 @@ let colors = [
     '#73A857'
 ];
 
-const tweetquote = document.getElementById('tweet-quote');
+
+const tmblr = document.getElementById('tumblr-quote');
+function updateTumblerLink(text, author) {
+    //sharedQuote = `freecodecamp&caption=Robert%20Frost&content=Two%20roads%20diverged%20in%20a%20wood%2C%20and%20I%E2%80%94I%20took%20the%20one%20less%20traveled%20by%2C%20And%20that%20has%20made%20all%20the%20difference.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`;
+    let link = `https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=${encodeURIComponent(author)}&content=${encodeURIComponent(text)}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`;
+    tmblr.href = link;
+}
+// https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Robert%20Frost&content=Two%20roads%20diverged%20in%20a%20wood%2C%20and%20I%E2%80%94I%20took%20the%20one%20less%20traveled%20by%2C%20And%20that%20has%20made%20all%20the%20difference.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button
 
 //when the twitter icon is pressed, user is routed to their page and is shown the random quote
+const tweetquote = document.getElementById('tweet-quote');
 function updateLinkTweet(text) {
-    
     
     let link = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURIComponent(text)}`;
 
     tweetquote.href = link;
-
-    // https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22Two%20roads%20diverged%20in%20a%20wood%2C%20and%20I%E2%80%94I%20took%20the%20one%20less%20traveled%20by%2C%20And%20that%20has%20made%20all%20the%20difference.%22%20Robert%20Frost
     
 }
 
 let btnClick=(e)=> {
-    console.log('something')
-    console.log(e);
 
     const QUOTES_API = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 
@@ -77,6 +80,7 @@ let btnClick=(e)=> {
         text.innerText = allInfo[randomNumber].quote;
         author.innerText = allInfo[randomNumber].author;
         updateLinkTweet(allInfo[randomNumber].quote);
+        updateTumblerLink(allInfo[randomNumber].author, allInfo[randomNumber].quote);
 
         // extract the quote, put the quote in the #text paragraph
     });
